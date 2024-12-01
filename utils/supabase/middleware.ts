a1dfdbc9c88ee8302ae5487b11376ece0ca857b5
xepiqq <jaydencrowther@gmail.com>
2024-12-01 00:38:51 -0700

    how bout dis for a test

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -40,9 +40,9 @@ export const updateSession = async (request: NextRequest) => {
     const user = await supabase.auth.getUser();
 
     // protected routes
-    if (request.nextUrl.pathname.startsWith("/protected") && user.error) {
-      return NextResponse.redirect(new URL("/sign-in", request.url));
-    }
+    // if (request.nextUrl.pathname.startsWith("/protected") && user.error) {
+    //   return NextResponse.redirect(new URL("/sign-in", request.url));
+    // }
 
     if (request.nextUrl.pathname === "/" && !user.error) {
       return NextResponse.redirect(new URL("/protected", request.url));
