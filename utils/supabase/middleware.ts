5bcc5bc407b3bac89fee586a7e8490180c2a0105
xepiqq <jaydencrowther@gmail.com>
2024-12-01 00:48:41 -0700

    very specific error

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -44,6 +44,14 @@ export const updateSession = async (request: NextRequest) => {
     //   return NextResponse.redirect(new URL("/sign-in", request.url));
     // }
 
+    if (
+      request.nextUrl.pathname.startsWith("/protected") &&
+      (!user.data || user.error?.name === "AuthSessionMissingError")
+    ) {
+      return NextResponse.redirect(new URL("/sign-in", request.url));
+    }
+        
+
     if (request.nextUrl.pathname === "/" && !user.error) {
       return NextResponse.redirect(new URL("/protected", request.url));
     }
